39cc8243eae16f584e5ae69717694b0ff76ce75e
romacw1484 <wromac@fordham.edu>
2024-09-11 13:10:39 -0400

    more fix

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var socket = io.connect('https://' + document.domain + ':' + location.port);
 
     socket.on('connect', () => {
         socket.emit('join', { 'username': currentUser });
